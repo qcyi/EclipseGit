c4fdef36cae41dff31ebed733bbab7fdf182d104
CHAOS <q.cyi2305@gmail.com>
2023-01-16 02:04:48 +0100

    Fichero Test01

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author chyrs
+ *
+ */
+module proyectoEclipseGit {
+}
---
+++ src/proyectoEclipseGit/Test01.java
@@ -0,0 +1,9 @@
+package proyectoEclipseGit;
+
+public class Test01 {
+	
+	public static void main(String[] args) {
+		System.out.println("Apartado 4");
+	}
+
+}
